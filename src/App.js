5bc2bcb0372f16f695521b20c1696e5470569caf
fhx10012091 <1875934025@qq.com>
2020-07-06 12:21:32 +0800

    init complete PriceList

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import React from 'react';
-
-function App() {
+import 'bootstrap/dist/css/bootstrap.min.css'
+import PriceList from './components/PriceList'
+const items = [
+  {
+    id: '1',
+    title: '去云南旅游',
+    data: '2018-08-08',
+    price: 200,
+    category: {
+      id: '1',
+      name: '旅游',
+      type: 'outcome'
+    }
+  }
+]
+const App = () => {
   return (
     <div className="App">
-      
+      <PriceList 
+      items={items} 
+      onModifyItem={(item) => alert(item.id)}
+      onDeleteItem={(item) => alert(item.id)}
+      />
     </div>
   );
 }
---
+++ src/components/PriceList.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const PriceList = ({items, onModifyItem, onDeleteItem}) => {
+    return (
+        <ul className="list-group list-group-flush">
+            {
+                items.map((item) => (
+                    <li className="list-group-item d-flex 
+                    justify-content-between align-items-center"
+                    key={item.id}>
+                        <span className="col-1">{item.category.name}</span>
+                        <span className="col-5">{item.title}</span>
+                        <span className="col-2 font-weight-bold">
+                            {(item.category.type==='outcome')? '-': '+'}
+                            { item.price}元
+                        </span>
+                        <span className="col-2">{item.date}</span>
+                        <button className="col-1 btn btn-primary" onClick={() => onModifyItem(item)}>编辑</button>
+                        <button className="col-1 btn btn-danger" onClick={() => onDeleteItem(item)}>删除</button>
+                        
+                    </li>
+                ))
+            }
+        </ul>
+    )
+}
+export default PriceList
